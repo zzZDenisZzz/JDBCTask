8a2d1c438a33228169037e222ee183d7abd537a6
zzZDenisZzz <corey2007@bigmir.net>
2018-10-20 16:25:50 +0300

    Add Product

---
+++ src/main/java/com/shop_kiev/Product.java
@@ -0,0 +1,15 @@
+package com.shop_kiev;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+
+}
